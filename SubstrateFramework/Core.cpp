bebb74940c56ba08163b41a6930d1e5dc7235bf5
Joe Haske <barbaricrunner@gmail.com>
2019-06-08 07:56:35 -0700

    Added comments and reorganized some code.

--- SubstrateFramework/Core.cpp
+++ SubstrateFramework/Core.cpp
@@ -17,6 +17,13 @@ namespace ssfw
 			Logger::printErrMsg("GLFW failed to initialize!", 10);
 		}
 
+		//Setup OpenGL Version and Options
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+
+		//Create a window using GLFW
 		window = glfwCreateWindow(640, 480, "SubstrateFrameworkGame", NULL, NULL);
 
 		if (!window)
@@ -33,12 +40,6 @@ namespace ssfw
 			Logger::printErrMsg("GLEW failed to initialize!", 10);
 		}
 
-		//Set OpenGL Settings
-		glClearColor(0.f, 0.f, 0.f, 1.f);
-
-
-		Triangle t(Vec3D<float>(-0.5f, -0.5f, 1.f), Vec3D<float>(0.f, 0.5f, 1.f), Vec3D<float>(0.5f, 0.5f, 1.f));
-
 		float positions[] =
 		{
 			-0.5f, -0.5f, 0.f,
@@ -48,23 +49,34 @@ namespace ssfw
 		};
 		unsigned int indices[] = { 0, 1, 2, 2, 3, 0 };
 
-		uint32_t shader = compileShaders(loadShaderFile("VertShader.shader"), loadShaderFile("FragShader.shader"));
+		unsigned int vertArray, vertBuffer, indBuffer;
 
-		unsigned int vertBuffer, indBuffer;
+		//Vertex Array Setup
+		glGenVertexArrays(1, &vertArray);
+		glBindVertexArray(vertArray);
 
-		//Vertex Buffer
+		//Vertex Buffer Setup
 		glGenBuffers(1, &vertBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, vertBuffer);
 
 		glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-		//Index Buffer
+
+		//Index Buffer Setup
 		glGenBuffers(1, &indBuffer);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBuffer);
 
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-		//Shader
+		
+		//Shader Setup
+		uint32_t shader = compileShaders(loadShaderFile("VertShader.shader"), loadShaderFile("FragShader.shader"));
+
+		//Clear the buffers
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 		while (!glfwWindowShouldClose(window))
 		{
@@ -74,6 +86,7 @@ namespace ssfw
 			
 			int location = glGetUniformLocation(shader, "vertColor");
 			glUniform4f(location, 0.f, 1.f, 0.9f, 1.f);
+			glBindVertexArray(vertArray);
 
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
